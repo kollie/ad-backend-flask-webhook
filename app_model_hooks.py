549f53c3cd8def91f34557459d1a5bdfbb8019da
kollie <zikollie@gmail.com>
2025-02-22 23:21:34 +0100

    added webhook endpoint

--- app_model_hooks.py
+++ app_model_hooks.py
@@ -82,8 +82,8 @@ def webhook():
     # path_repo = "/route/to/your/repository/on/PythonAnywhere"
     # servidor_web = "/route/to/the/WSGI/file/for/configuration"
 
-    path_repo = "/home/prueba83/flask-project/ad-backend"
-    servidor_web = "/var/www/prueba83_pythonanywhere_com_wsgi.py"
+    path_repo = "/home/kollie/flask-project/ad-backend"
+    servidor_web = "/var/www/kollie_pythonanywhere_com_wsgi.py"
 
     # It checks if the POST request has JSON data
     if request.is_json:
